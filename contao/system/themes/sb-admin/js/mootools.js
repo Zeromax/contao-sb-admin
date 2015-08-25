314326f6333de876a4e718511ed6928a1019b2d6
Andreas Nölke <zero@brothers-project.de>
2015-08-25 17:21:24 +0200

    remove useless comment

--- contao/system/themes/sb-admin/js/mootools.js
+++ contao/system/themes/sb-admin/js/mootools.js
@@ -47,11 +47,4 @@
 
 		return false;
 	}
-};/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-
+};
--- js/components/m_custom.js
+++ js/components/m_custom.js
@@ -46,10 +46,4 @@ var BsAjaxRequest =
 
 		return false;
 	}
-};/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+};
