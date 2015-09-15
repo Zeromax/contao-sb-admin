648095e6c15d33c6f37ec2fab75048a983d70953
Andreas Nölke <zero@brothers-project.de>
2015-09-15 23:59:57 +0200

    fix broken Ajax Request cause of icon font replacement

--- js/components/contao/core-uncompressed.js
+++ js/components/contao/core-uncompressed.js
@@ -1,6 +1,4 @@
-//@prepros-append components/contao/core-uncompressed.js
-//@prepros-append components/m_custom.js
-;/**
+/**
  * Contao Open Source CMS
  *
  * Copyright (c) 2005-2015 Leo Feyer
@@ -2259,54 +2257,3 @@ window.addEvent('ajax_change', function() {
 		}).chosen();
 	}
 });
-
-var BsAjaxRequest =
-{
-	/**
-	 * Toggle the navigation menu
-	 *
-	 * @param {object} el The DOM element
-	 * @param {string} id The ID of the menu item
-	 *
-	 * @returns {boolean}
-	 */
-	toggleNavigation: function(el, id) {
-		el.blur();
-
-		var item = $(id);
-
-		if (item) {
-			if (item.get('aria-expanded') == 'false') {
-				$(el).store('tip:title', Contao.lang.collapse);
-				new Request.Contao({
-					evalScripts: true,
-					onRequest: AjaxRequest.displayBox(Contao.lang.loading + ' …'),
-					onSuccess: function(txt) {
-						var item = $(id);
-						item.set("html", txt);
-
-						// Update the referer ID
-						item.getElements('a').each(function(el) {
-							el.href = el.href.replace(/&ref=[a-f0-9]+/, '&ref=' + Contao.referer_id);
-						});
-
-						$(el).store('tip:title', Contao.lang.collapse);
-						AjaxRequest.hideBox();
-
-						// HOOK
-						window.fireEvent('ajax_change');
-					}
-				}).post({'action':'loadNavigation', 'id':id, 'state':1, 'REQUEST_TOKEN':Contao.request_token});
-			} else {
-				$(el).store('tip:title', Contao.lang.expand);
-				new Request.Contao().post({'action':'toggleNavigation', 'id':id, 'state':0, 'REQUEST_TOKEN':Contao.request_token});
-			}
-			return false;
-		}
-
-
-
-		return false;
-	}
-};
-Tips.Contao = new Class ({});
--- js/mootools.js
+++ js/mootools.js
@@ -1,1 +1,2 @@
+//@prepros-append components/contao/core-uncompressed.js
 //@prepros-append components/m_custom.js
