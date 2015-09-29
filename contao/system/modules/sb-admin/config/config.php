1e231b34172fb27766324acde11eb2c36532845c
Andreas Nölke <zero@brothers-project.de>
2015-09-29 19:41:50 +0200

    setup TL_ASSETS config array for contao 3.2

--- contao/system/modules/sb-admin/config/config.php
+++ contao/system/modules/sb-admin/config/config.php
@@ -16,3 +16,29 @@ if (TL_MODE == "BE")
 	$GLOBALS['TL_HOOKS']['parseTemplate'][] = array('SbTemplate', 'parseTemplate');
 }
 $GLOBALS['TL_HOOKS']['initializeSystem'][] = array('SbTemplate', 'chooseTemplates');
+
+if (version_compare(VERSION, '3.2', '<='))
+{
+	/** Backwards compatibility for old contao */
+	$GLOBALS['TL_ASSETS'] = array
+	(
+		'ACE'          => ACE,
+		'CSS3PIE'      => CSS3PIE,
+		'DROPZONE'     => DROPZONE,
+		'HIGHLIGHTER'  => HIGHLIGHTER,
+		'HTML5SHIV'    => HTML5SHIV,
+		'RESPIMAGE'    => RESPIMAGE,
+		'SWIPE'        => SWIPE,
+		'JQUERY'       => JQUERY,
+		'JQUERY_UI'    => JQUERY_UI,
+		'COLORBOX'     => COLORBOX,
+		'MEDIAELEMENT' => MEDIAELEMENT,
+		'TABLESORTER'  => TABLESORTER,
+		'MOOTOOLS'     => MOOTOOLS,
+		'COLORPICKER'  => COLORPICKER,
+		'DATEPICKER'   => DATEPICKER,
+		'MEDIABOX'     => MEDIABOX,
+		'SIMPLEMODAL'  => SIMPLEMODAL,
+		'SLIMBOX'      => SLIMBOX
+	);
+}
